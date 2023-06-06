250cd5c385935da49eb9c20dd398b0cf73b7f5d0
Elizaveta Khaletskaya <effy4814@gmail.com>
2023-06-06 13:04:35 +0300

    src/

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int initial = 500;
+        int sum = 1500;
+
+
+        int bonus;
+        if (sum > 1000) {
+            bonus = ( sum / 100 );
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Сумма бонуса: " + bonus );
+        System.out.println("Итоговая сумма: " + (initial + sum + bonus) );
 
     }
 }
